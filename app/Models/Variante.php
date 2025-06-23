26e7beb2394ced80f47f54a6fe1a0e7b76fabfb9
carlos.alvarado <carlos.alvarado@ct.uneatlantico.es>
2025-06-24 00:16:05 +0200

    Add Variante model with relationships and fillable attributes; update Producto model to include variantes relationship.

--- app/Models/Variante.php
+++ app/Models/Variante.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Variante extends Model
 {
-    //
+    protected $table = 'variantes';
+    protected $primaryKey = 'id_variantes';
+    public $timestamps = true;
+    protected $fillable = [
+        'id_producto',
+        'peso_libras',
+        'precio_unitario',
+        'precio_por_mayor',
+        'stock'
+    ];
+    protected $hidden = ['created_at', 'updated_at'];
+    public function productos(): BelongsTo 
+    {
+        return $this->belongsTo(Producto::class, 'id_producto');
+    }
 }
